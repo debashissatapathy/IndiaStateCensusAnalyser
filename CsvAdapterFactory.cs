e8e64af4e89a0ad76894e3a0fda5279f200643d9
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-06 21:57:18 +0530

    [Debashis] India State census analyser information

---
+++ CsvAdapterFactory.cs
@@ -0,0 +1,24 @@
+﻿using IndiaStateCensusAnalyser.POCO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace IndiaStateCensusAnalyser                              
+{
+    public class CsvAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string CsvFilePath, string dataheaders)
+        {
+            switch(country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndiaCensusAdapter().LoadCensusData(CsvFilePath, dataheaders);
+                    
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+
+        }
+    }
+}
