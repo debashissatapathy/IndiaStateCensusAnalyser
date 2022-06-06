129fbc77c8e72b1ce4d0f527ac2a15bb38071b85
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-06 21:53:09 +0530

    [Debashis] India State census analyser information

---
+++ CensusAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using IndiaStateCensusAnalyser.POCO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndiaStateCensusAnalyser
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, USA, BRAZIL
+        }
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusMap(Country country, string CsvFilePath, string dataHeaders)
+        {
+            dataMap = new CsvAdapterFactory().LoadCsvData(country, CsvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+        
+    }
+}
